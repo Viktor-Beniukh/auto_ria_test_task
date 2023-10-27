48675829692fbb1e41bc9f3ce53df4778c60dbd1
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-10-27 17:58:15 +0300

    Set time for tasks

--- celery_config.py
+++ celery_config.py
@@ -10,10 +10,10 @@ app.conf.timezone = "Europe/Kiev"
 app.conf.beat_schedule = {
     "run-script-every-day": {
         "task": "tasks.insert_or_update_data_cars_to_db",
-        "schedule": crontab(hour="9", minute="45"),
+        "schedule": crontab(hour="12", minute="0"),
     },
     "backup-data-every-day": {
         "task": "tasks.backup_cars_data",
-        "schedule": crontab(hour="10", minute="0"),
+        "schedule": crontab(hour="0", minute="0"),
     },
 }
